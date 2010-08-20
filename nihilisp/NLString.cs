39e9cfda36401f14978001aba1c764b9ebef72ab
seth-schroeder <foogoof@gmail.com>
2010-08-20 00:00:16 -0400

    YA baby step

---
+++ nihilisp/NLString.cs
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2010 Seth Schroeder
+// This file is part of Nihilisp.
+//
+// Nihilisp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nihilisp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Nihilisp.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
+// -*- mode: csharp -*-
+////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+using foognostic.nihilisp.core;
+
+namespace nihilisp
+{
+	public class NLString : NLObject, IAtom
+	{
+		string val;
+		
+		public NLString ()
+		{
+		}
+		
+		public NLString create(string str)
+		{
+			NLString inst = new NLString();
+			inst.val = str;
+			return inst;
+		}
+	}
+}
+
